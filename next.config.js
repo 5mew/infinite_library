d8cccc01489d8730a1428d209f2947ddf92eba9c
5mew <76901333+5mew@users.noreply.github.com>
2025-05-31 14:40:34 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    fontLoaders: [
-      { loader: '@next/font/google', options: { subsets: ['latin'] } },
-    ],
-  },
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
 }
 
 module.exports = nextConfig

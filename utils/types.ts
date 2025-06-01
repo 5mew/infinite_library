9c335dd96f915877b2da8698b0767bceeae3f3b6
5mew <76901333+5mew@users.noreply.github.com>
2025-06-01 08:28:33 -0500

    Create types.ts

---
+++ utils/types.ts
@@ -0,0 +1,28 @@
+// utils/types.ts
+export interface UserProfile {
+  name: string;
+  age: string;
+  interests: string[];
+  readingLevel: string;
+  preferredGenre: string;
+  personalityTraits: string[];
+  currentMood: string;
+  location: string;
+  personalChallenges: string[];
+  favoriteBooks: string;
+  dreamScenario: string;
+  personalStory: string;
+  creativeTrigger: string;
+}
+
+export interface Book {
+  id: string;
+  title: string;
+  author: string;
+}
+
+export interface Chapter {
+  id: string;
+  title: string;
+  content: string;
+}
